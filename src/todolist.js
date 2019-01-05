458a3b3729ec337c5f6a9a453fdf9e9d98195ab1
Chitresh <chitresh@goscale.co>
2019-01-05 17:06:55 +0530

    commit table css

--- src/todolist.js
+++ src/todolist.js
@@ -1,31 +1,37 @@
 import React from 'react'
+import {Button, Grid} from "@material-ui/core";
 
 
 const ToDoList = () => {
-    return(
-        <div className = "container">
+    return (
+        <div className="container">
             <div className="scores-block1 clearfix">
-<div className="bg-white shadow-sm Assessment_Results">
-<h5 style={{textAlign : "center"}}>Page : </h5>
-<button type="button" className="page-link" style ={{order : "3", marginLeft:"43%",borderRadius:"3px",right:"5px",padding:"3px 13px",display:"inline"}} name = "left" >Previous</button>
-<button type="button" className="page-link" style ={{order : "3", left:"5px",borderRadius:"3px",padding:"3px 13px",display:"inline"}} name="right" >Next</button>
-
- <table className="table">
-   <thead >
-     <tr >
-       <th> <span className="icon-question-circle1" />Name</th>
-       <th> <span className="icon-clock" />Estimated Time</th>
-       <th> <span className="icon-clock" />Start Time</th>
-       <th> <span className="icon-clock" />End Time</th>
-       <th> <span className="icon-clock" />Total Time</th>
-     </tr>
-   </thead>
-   <tbody>
-   </tbody>
- </table>
-</div>
-</div>
-</div>
+                <div className="bg-white shadow-sm Assessment_Results">
+                    <h5 style={{ textAlign: "center" }}>Add Item </h5>
+                    <Grid container style={{ marginTop: '10%', textAlign: 'center' }}>
+                    <Grid item xs={12} md={9} lg={6} style={{ margin: '0 auto' }}>
+                        <input type = "text" />
+                    <Button variant="contained" color="primary">
+                    Submit
+                    </Button>
+                    </Grid>
+                    </Grid>
+                    <table className="table">
+                        <thead >
+                            <tr >
+                                <th> <span className="icon-question-circle1" />Name</th>
+                                <th> <span className="icon-clock" />Estimated Time</th>
+                                <th> <span className="icon-clock" />Start Time</th>
+                                <th> <span className="icon-clock" />End Time</th>
+                                <th> <span className="icon-clock" />Total Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     )
 }
 
