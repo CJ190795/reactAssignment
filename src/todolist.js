76dd460b16177b5cb8333c2568ddb2ca24e1a1db
Chitresh <chitresh@goscale.co>
2019-01-05 17:53:47 +0530

    dummy data

--- src/todolist.js
+++ src/todolist.js
@@ -1,31 +1,31 @@
 import React from 'react'
-import {Button, Grid} from "@material-ui/core";
-
+import './App.css'
 
 const ToDoList = () => {
+
+    
     return (
         <div className="container">
             <div className="scores-block1 clearfix">
                 <div className="bg-white shadow-sm Assessment_Results">
-                <div>
-                <h5 style={{ textAlign: "center" }}>Add Todos </h5>
-                <div >
-                    <label>Todo :  </label>
-                    <div >
-                        <input type="text" name="todo" placeholder="Add Todo" />
+                <div style={{textAlign : "center"}}>
+                <div className="form-group row" >
+                    <label className= "col-sm-3 col-form-label">Todo :  </label>
+                    <div className="col-sm-6">
+                        <input className="form-control" type="text" name="todo" placeholder="Add Todo" />
                     </div>
                 </div>
-                <div >
-                    <label>Estimated Time : </label>
-                    <div>
-                        <input type="text" name="time" placeholder="Estimated time" />
+                <div className="form-group row">
+                    <label className = "col-sm-3 col-form-label">Estimated Time : </label>
+                    <div className="col-sm-6">
+                        <input className="form-control" type="text" name="time" placeholder="Estimated time" />
                     </div>
                 </div>
-                    <Button variant="contained" color="primary">
+                    <button className="btn btn-primary btn-lg" type = "button" variant="contained" color="primary">
                     Submit
-                    </Button>
+                    </button>
                 </div>    
-                    <table className="table">
+                    <table className="table" style= {{marginTop : "2rem"}}>
                         <thead >
                             <tr >
                                 <th> <span className="icon-question-circle1" />Name</th>
@@ -35,13 +35,27 @@ const ToDoList = () => {
                                 <th> <span className="icon-clock" />Total Time</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody >
+                        <tr>
+                            <td>
+                            <div>
+                                <p>
+                                Clean Dishes
+                                </p>
+                                </div>
+                            </td>
+                            <td>20</td> 
+                            <td>12:10:05</td>
+                            <td>12:17:00</td>
+                            <td>00:06:55</td>
+                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-    )
+    );
+    
 }
 
 export default ToDoList;
