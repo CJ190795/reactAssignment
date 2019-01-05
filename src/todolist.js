44c221d4725e1785979b839394427c76699a2aac
Chitresh <chitresh@goscale.co>
2019-01-05 16:49:05 +0530

    second commit table add

---
+++ src/todolist.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const ToDoList = () => {
+    return(
+        <div className = "container">
+            <div className="scores-block1 clearfix">
+<div className="bg-white shadow-sm Assessment_Results">
+<h5 style={{textAlign : "center"}}>Page : </h5>
+<button type="button" className="page-link" style ={{order : "3", marginLeft:"43%",borderRadius:"3px",right:"5px",padding:"3px 13px",display:"inline"}} name = "left" >Previous</button>
+<button type="button" className="page-link" style ={{order : "3", left:"5px",borderRadius:"3px",padding:"3px 13px",display:"inline"}} name="right" >Next</button>
+
+ <table className="table">
+   <thead >
+     <tr >
+       <th> <span className="icon-question-circle1" />Name</th>
+       <th> <span className="icon-comment-alt" />Estimated Time</th>
+       <th> <span className="icon-clock" />Start Time</th>
+       <th> <span className="icon-star-half-alt" />End Time</th>
+       <th> <span className="icon-star-half-alt" />Total Time</th>
+     </tr>
+   </thead>
+   <tbody>
+   </tbody>
+ </table>
+</div>
+</div>
+</div>
+    )
+}
+
+export default ToDoList;
