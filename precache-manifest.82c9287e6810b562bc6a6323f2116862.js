85f5afab07599f93c9930aa77935a4a1e7b2ce82
Chitresh <chitresh@goscale.co>
2019-01-05 17:09:40 +0530

    Updates

---
+++ precache-manifest.82c9287e6810b562bc6a6323f2116862.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc64e90d3a37e5ba92e3",
+    "url": "/reactAssignment/static/js/runtime~main.bc64e90d.js"
+  },
+  {
+    "revision": "9bbc223e38b96a423d1d",
+    "url": "/reactAssignment/static/js/main.9bbc223e.chunk.js"
+  },
+  {
+    "revision": "2cc43beffc44334d3bc3",
+    "url": "/reactAssignment/static/js/1.2cc43bef.chunk.js"
+  },
+  {
+    "revision": "9bbc223e38b96a423d1d",
+    "url": "/reactAssignment/static/css/main.cd7179b0.chunk.css"
+  },
+  {
+    "revision": "4fe2a292c16f94bd87a8d849f4771e84",
+    "url": "/reactAssignment/index.html"
+  }
+];
